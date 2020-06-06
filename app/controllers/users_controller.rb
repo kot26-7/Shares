5443a47a5bbe7ca95454d90f0560184ad7e41ce3
kot26-7 <taro726896oka@gmail.com>
2020-06-06 15:02:46 +0900

    1 create-users-controller, 2 create-user-image-uploader-and-edit-config-to-upload-image, 3 change-registration-edit-layouts-as-password-change,4 create-resourceful-users-actions-layouts

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user =User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user), alert: "Invalid access detected"
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "Update Your Profile successfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:notice] = "User deleted"
+    redirect_to users_path
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email,:fullname,:phonenumber, :gender, :introduce, :website, :profile_image)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	mount_uploader :profile_image, ProfileImageUploader
 	#validates :username, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
