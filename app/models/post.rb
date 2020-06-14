24820f95038c11e60a4e01249f8a8313c0be56e7
kot26-7 <taro726896oka@gmail.com>
2020-06-14 17:43:04 +0900

    trying to fix error select-distinct when create notification

--- app/models/post.rb
+++ app/models/post.rb
@@ -28,7 +28,7 @@ class Post < ApplicationRecord
 
   #notifications of comments
   def create_notification_comment!(current_user, comment_id)
-    temp_ids = Comment.where(post_id: id).where.not("user_id=? or user_id=?", current_user.id,user_id).select(:user_id).distinct
+    temp_ids = Comment.select(:user_id).where(post_id: id).where.not(user_id: current_user.id).distinct
     temp_ids.each do |temp_id|
       save_notification_comment!(current_user, comment_id, temp_id['user_id'])
     end
